7830fc70e9268001d76259936fff2cb321a10307
Karim Abou Nassif <karimabounassif@Karim-Abou-Nassifs-MacBook-Pro.local>
2018-06-27 10:09:23 -0500

    Database connected

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "e3a545jf";
+    private static final String CONN_STRING = "jdbc:mysql://localhost/week1?useSSL=false&useUnicode=true" +
+            "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+    public static void main(String[] args) throws SQLException {
+
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+            System.out.println("Connected");
+        } catch (SQLException e) {
+            System.err.println(e);
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+        }
+
+    }
 }
